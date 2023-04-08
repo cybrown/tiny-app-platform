77ac441c5c7c5e663595aea314d15c13d9d33adb
Cy Brown <cy.brown59@gmail.com>
2023-04-08 23:28:22 +0200

    fix(image): Fixed default image width, added a prop to fit container

--- packages/web-client/src/widgets/Image.tsx
+++ packages/web-client/src/widgets/Image.tsx
@@ -4,15 +4,26 @@ import styles from "./Image.module.css";
 type ImageProps = {
   ctx: RuntimeContext;
   url: string;
+  fit?: boolean;
 };
 
-export default function Image({ ctx, url }: ImageProps) {
-  return <img className={styles.Image} src={url} alt="Default content" />;
+export default function Image({ ctx, url, fit }: ImageProps) {
+  return (
+    <div>
+      <img
+        className={`${styles.Image} ${fit ? styles.fitted : ""}`}
+        src={url}
+        alt="Default content"
+      />
+    </div>
+  );
 }
 
 export const ImageDocumentation: WidgetDocumentation<ImageProps> = {
   description: "An image from a URL",
   props: {
     url: "URL to the image",
+    fit:
+      "Allow the image to use its container size, use inside a sized Box to control the image size",
   },
 };

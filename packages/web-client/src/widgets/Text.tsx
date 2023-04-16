a0c78fae285aec1e335c61cd89fb18c71833b70b
Cy Brown <cy.brown59@gmail.com>
2023-04-16 19:09:10 +0200

    feat(text): Added more props for Text widget

--- packages/web-client/src/widgets/Text.tsx
+++ packages/web-client/src/widgets/Text.tsx
@@ -1,4 +1,4 @@
-import { useCallback } from "react";
+import { useCallback, useMemo } from "react";
 import { RuntimeContext, WidgetDocumentation } from "tal-eval";
 import styles from "./Text.module.css";
 
@@ -7,13 +7,55 @@ type TextProps = {
   text: string;
   preformatted: boolean;
   copy?: boolean;
+  size?: number;
+  align?: "center" | "right" | "left";
+  weight?: "light" | "normal" | "bold";
+  wrap?: boolean;
+  color?: string;
 };
 
-export default function Text({ ctx, copy, preformatted, text }: TextProps) {
+export default function Text({
+  ctx,
+  copy,
+  preformatted,
+  text,
+  size,
+  align,
+  color,
+  weight = "normal",
+  wrap = false,
+}: TextProps) {
+  if (
+    preformatted &&
+    (size != null ||
+      align != null ||
+      weight != null ||
+      wrap != null ||
+      color != null)
+  ) {
+    throw new Error(
+      "preformatted is not compatible with size, align, weight, wrap or color"
+    );
+  }
+
   const showCopyButton = copy ?? preformatted;
+
   const copyClickHandler = useCallback(() => {
     navigator.clipboard.writeText(text);
   }, [text]);
+
+  const style = useMemo<React.CSSProperties>(
+    () => ({
+      fontSize: (size ?? 1) + "em",
+      textAlign: align ?? "left",
+      fontFamily: weight === "light" ? "Proxima Nova Lt" : "Proxima Nova Rg",
+      fontWeight: weight,
+      whiteSpace: wrap ? undefined : "nowrap",
+      color: color,
+    }),
+    [align, size, weight, wrap, color]
+  );
+
   return (
     <div className={styles.Text}>
       {showCopyButton ? (
@@ -24,7 +66,9 @@ export default function Text({ ctx, copy, preformatted, text }: TextProps) {
       {preformatted ? (
         <pre>{text}</pre>
       ) : (
-        <div className={styles.Text}>{text}</div>
+        <div className={styles.Text} style={style}>
+          {text}
+        </div>
       )}
     </div>
   );
@@ -37,5 +81,10 @@ export const TextDocumentation: WidgetDocumentation<TextProps> = {
       "Show a button to copy the content, default true when preformatted is true",
     preformatted: "Show text with a monospace font and keep whitespaces",
     text: "Text to display",
+    size: "Size in proportion of base size",
+    align: "Text alignement: center, left, right",
+    weight: "Font weight: light, normal, bold",
+    wrap: "True if text must wrap, false by default",
+    color: "Text color",
   },
 };

07e3372cc329bf1febb14f09b6d626781bb5a541
Cy Brown <cy.brown59@gmail.com>
2023-05-09 01:35:05 +0200

    feat(table): Better Table style, added bordered and striped props

--- packages/web-client/src/widgets/Table.tsx
+++ packages/web-client/src/widgets/Table.tsx
@@ -1,6 +1,7 @@
 import { FunctionValue, RuntimeContext, WidgetDocumentation } from "tal-eval";
 import RenderExpression from "../runtime/RenderExpression";
 import styles from "./Table.module.css";
+import Debug from "./Debug";
 
 type TableModelColumn = {
   description: string;
@@ -11,14 +12,40 @@ type TableProps = {
   ctx: RuntimeContext;
   values: unknown[];
   columns: TableModelColumn[];
+  bordered: boolean;
+  striped: boolean;
 };
 
-export default function Table({ ctx, columns, values }: TableProps) {
+export default function Table({
+  ctx,
+  columns,
+  values,
+  bordered,
+  striped,
+}: TableProps) {
+  let effectiveColumns: any[] = [];
+  if (!columns) {
+    const columnsToAdd = new Set<string>();
+    values.forEach((value) => {
+      if (value && typeof value == "object") {
+        Object.keys(value).forEach((name) => columnsToAdd.add(name));
+      }
+    });
+    columnsToAdd.forEach((name) => {
+      effectiveColumns.push({ description: name });
+    });
+  } else {
+    effectiveColumns = columns;
+  }
   return (
-    <table className={styles.Table}>
+    <table
+      className={`${styles.Table} ${bordered ? styles.bordered : ""} ${
+        striped ? styles.striped : ""
+      }`}
+    >
       <thead>
         <tr>
-          {(columns ?? []).map((col) => (
+          {(effectiveColumns ?? []).map((col) => (
             <th key={col.description}>{col.description}</th>
           ))}
         </tr>
@@ -27,15 +54,19 @@ export default function Table({ ctx, columns, values }: TableProps) {
         {(values ?? []).map((value, index) => {
           return (
             <tr key={index}>
-              {(columns ?? []).map((col) => (
+              {(effectiveColumns ?? []).map((col) => (
                 <td key={col.description}>
-                  <RenderExpression
-                    ctx={ctx}
-                    expression={{
-                      kind: "Value",
-                      value: ctx.callFunction(col.display, [value]),
-                    }}
-                  />
+                  {col.display ? (
+                    <RenderExpression
+                      ctx={ctx}
+                      expression={{
+                        kind: "Value",
+                        value: ctx.callFunction(col.display, [value]),
+                      }}
+                    />
+                  ) : (value as any)[col.description] !== undefined ? (
+                    <Debug value={(value as any)[col.description]} />
+                  ) : null}
                 </td>
               ))}
             </tr>
@@ -52,5 +83,7 @@ export const TableDocumentation: WidgetDocumentation<TableProps> = {
     columns:
       "Column definition, array of description (string) and display (function that takes one entry and renders its cell)",
     values: "Array of entries",
+    bordered: "Add borders",
+    striped: "Alternate background color",
   },
 };

86a4434aadb9e64fdb5f61d70021b85bc513d979
Cy Brown <cy.brown59@gmail.com>
2024-01-06 23:12:19 +0100

    feat(string): Added ignoreCase parameter to string_contains

--- packages/tal-stdlib/src/functions/string.ts
+++ packages/tal-stdlib/src/functions/string.ts
@@ -1,121 +1,126 @@
-import { defineFunction, RuntimeContext } from "tal-eval";
-import { base64_to_bytes } from "../util/base64";
+import { defineFunction, RuntimeContext } from 'tal-eval';
+import { base64_to_bytes } from '../util/base64';
 
 export const string_to_bytes = defineFunction(
-  "string_to_bytes",
-  [{ name: "string" }, { name: "encoding" }],
+  'string_to_bytes',
+  [{ name: 'string' }, { name: 'encoding' }],
   (_ctx, { string, encoding }) => {
-    const pEncoding = encoding ?? "utf-8"
+    const pEncoding = encoding ?? 'utf-8';
     switch (pEncoding) {
-      case "base64":
+      case 'base64':
         return base64_to_bytes(string);
-      case "base64url":
+      case 'base64url':
         return base64_to_bytes(string);
-      case "utf-8":
+      case 'utf-8':
         return new TextEncoder().encode(string);
       default:
-        throw new Error("Encoding not supported: " + pEncoding);
+        throw new Error('Encoding not supported: ' + pEncoding);
     }
   }
 );
 
 export const string_to_number = defineFunction(
-  "string_to_number",
-  [{ name: "string" }],
+  'string_to_number',
+  [{ name: 'string' }],
   (_ctx, { string }) => {
     return Number(string);
   }
 );
 
 export const string_split = defineFunction(
-  "string_split",
-  [{ name: "string" }, { name: "separator" }],
+  'string_split',
+  [{ name: 'string' }, { name: 'separator' }],
   (_ctx, { string, separator }) => {
     return string.split(separator);
   }
 );
 
 export const string_locale_compare = defineFunction(
-  "string_locale_compare",
-  [{ name: "a" }, { name: "b" }],
+  'string_locale_compare',
+  [{ name: 'a' }, { name: 'b' }],
   (_ctx, { a, b }) => a.localeCompare(b)
 );
 
 export const string_trim = defineFunction(
-  "string_trim",
-  [{ name: "string" }],
+  'string_trim',
+  [{ name: 'string' }],
   (_ctx, { string }) => (string as string).trim()
 );
 
 export const string_starts_with = defineFunction(
-  "string_starts_with",
-  [{ name: "string" }, { name: "search" }],
+  'string_starts_with',
+  [{ name: 'string' }, { name: 'search' }],
   (_ctx, { string, search }) => (string as string).startsWith(search)
 );
 
 export const string_ends_with = defineFunction(
-  "string_ends_with",
-  [{ name: "string" }, { name: "search" }],
+  'string_ends_with',
+  [{ name: 'string' }, { name: 'search' }],
   (_ctx, { string, search }) => (string as string).endsWith(search)
 );
 
 export const string_contains = defineFunction(
-  "string_contains",
-  [{ name: "string" }, { name: "search" }],
-  (_ctx, { string, search }) => (string as string).includes(search)
+  'string_contains',
+  [{ name: 'string' }, { name: 'search' }, { name: 'ignoreCase' }],
+  (_ctx, { string, search, ignoreCase }) =>
+    ignoreCase
+      ? (string as string)
+          .toLocaleLowerCase()
+          .includes(search.toLocaleLowerCase())
+      : (string as string).includes(search)
 );
 
 export const string_format = defineFunction(
-  "string_format",
-  [{ name: "template" }, { name: "values" }],
+  'string_format',
+  [{ name: 'template' }, { name: 'values' }],
   string_format_impl
 );
 
 export const string_lower = defineFunction(
-  "string_lower",
-  [{ name: "string" }],
+  'string_lower',
+  [{ name: 'string' }],
   (_ctx, { string }) => (string as string).toLocaleLowerCase()
 );
 
 export const string_upper = defineFunction(
-  "string_upper",
-  [{ name: "string" }],
+  'string_upper',
+  [{ name: 'string' }],
   (_ctx, { string }) => (string as string).toLocaleUpperCase()
 );
 
 export const string_slice = defineFunction(
-  "string_slice",
-  [{ name: "string" }, { name: "start" }, { name: "end" }],
+  'string_slice',
+  [{ name: 'string' }, { name: 'start' }, { name: 'end' }],
   (_ctx, { string, start, end }) => (string as string).slice(start, end)
 );
 
 export const string_repeat = defineFunction(
-  "string_repeat",
-  [{ name: "string" }, { name: "count" }],
+  'string_repeat',
+  [{ name: 'string' }, { name: 'count' }],
   (_ctx, { string, count }) => (string as string).repeat(count)
 );
 
 export const string_trim_start = defineFunction(
-  "string_trim_start",
-  [{ name: "string" }],
+  'string_trim_start',
+  [{ name: 'string' }],
   (_ctx, { string }) => (string as string).trimStart()
 );
 
 export const string_trim_end = defineFunction(
-  "string_trim_end",
-  [{ name: "string" }],
+  'string_trim_end',
+  [{ name: 'string' }],
   (_ctx, { string }) => (string as string).trimEnd()
 );
 
 export const string_pad_start = defineFunction(
-  "string_pad_start",
-  [{ name: "string" }, { name: "length" }, { name: "fill" }],
+  'string_pad_start',
+  [{ name: 'string' }, { name: 'length' }, { name: 'fill' }],
   (_ctx, { string, length, fill }) => (string as string).padStart(length, fill)
 );
 
 export const string_pad_end = defineFunction(
-  "string_pad_end",
-  [{ name: "string" }, { name: "length" }, { name: "fill" }],
+  'string_pad_end',
+  [{ name: 'string' }, { name: 'length' }, { name: 'fill' }],
   (_ctx, { string, length, fill }) => (string as string).padEnd(length, fill)
 );
 
@@ -125,7 +130,7 @@ const extractPlaceholdersRegexp = /\$[a-zA-Z_$]+[a-zA-Z_$0-9]*/g;
 function replacePlaceholdersByLocals(ctx: RuntimeContext, str: string) {
   return str.replace(
     extractPlaceholdersRegexp,
-    (name) => ctx.getLocalOr(name.substring(1), "") as string
+    name => ctx.getLocalOr(name.substring(1), '') as string
   );
 }
 
@@ -134,12 +139,12 @@ function replacePlaceholdersByExpressions(
   str: string,
   values: { [key: string]: string }
 ) {
-  return str.replace(extractPlaceholdersRegexp, (name) => {
+  return str.replace(extractPlaceholdersRegexp, name => {
     try {
       return values[name.substring(1)];
     } catch (err) {
       // TODO: Only return this when error is missing local
-      return "";
+      return '';
     }
   });
 }

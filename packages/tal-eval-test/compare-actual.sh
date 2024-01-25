bf468f85d90d3306417166409c57b6b5d3af3693
Cy Brown <cy.brown59@gmail.com>
2024-01-25 23:54:56 +0100

    fix(tests): Snaoshot tests will fail when some files are different

--- packages/tal-eval-test/compare-actual.sh
+++ packages/tal-eval-test/compare-actual.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
-ls test-sources/*.tas | while read -r path
+has_errors=false
+
+while read -r path
 do
   name_with_extension=$(basename "$path")
   name="${name_with_extension%.*}"
-  diff -q "actual/$name.sync.txt" "expected/$name.sync.txt"
-  diff -q "actual/$name.async.txt" "expected/$name.async.txt"
-  diff -q "actual/$name.ir.txt" "expected/$name.ir.txt"
-done
+  diff -q "actual/$name.sync.txt" "expected/$name.sync.txt" || has_errors=true
+  diff -q "actual/$name.async.txt" "expected/$name.async.txt" || has_errors=true
+  diff -q "actual/$name.ir.txt" "expected/$name.ir.txt" || has_errors=true
+done <<< $(ls test-sources/*.tas)
+
+if  [ $has_errors = true ]
+then
+  exit 1
+fi
--- packages/tal-parser-test/compare-actual.sh
+++ packages/tal-parser-test/compare-actual.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
-ls test-sources/*.tas | while read -r path
+has_errors=false
+
+while read -r path
 do
   name_with_extension=$(basename "$path")
   name="${name_with_extension%.*}"
-  diff -q "actual/$name.json" "expected/$name.json"
-  diff -q "actual/$name.tas" "expected/$name.tas"
-done
+  diff -q "actual/$name.json" "expected/$name.json" || has_errors=true
+  diff -q "actual/$name.tas" "expected/$name.tas" || has_errors=true
+done <<< $(ls test-sources/*.tas)
+
+if [ $has_errors = true ]
+then
+  exit 1
+fi

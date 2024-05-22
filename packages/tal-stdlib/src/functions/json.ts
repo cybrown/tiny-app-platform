74ab7501d4b72c02548113bd4d9f68c1dcc87303
Cy Brown <cy.brown59@gmail.com>
2024-05-22 18:15:03 +0200

    feat(json): Added format parameter to json_stringify

--- packages/tal-stdlib/src/functions/json.ts
+++ packages/tal-stdlib/src/functions/json.ts
@@ -1,25 +1,28 @@
-import { defineFunction } from "tal-eval";
-import { search } from "jmespath";
+import { defineFunction } from 'tal-eval';
+import { search } from 'jmespath';
 
 export const json_parse = defineFunction(
-  "json_parse",
-  [{ name: "string" }],
+  'json_parse',
+  [{ name: 'string' }],
   (_ctx, { string }) => {
     return JSON.parse(string);
   }
 );
 
 export const json_stringify = defineFunction(
-  "json_stringify",
-  [{ name: "any" }],
-  (_ctx, { any }) => {
+  'json_stringify',
+  [{ name: 'any' }, { name: 'format' }],
+  (_ctx, { any, format }) => {
+    if (format) {
+      return JSON.stringify(any, null, '  ');
+    }
     return JSON.stringify(any);
   }
 );
 
 export const jmespath_search = defineFunction(
-  "jmespath_search",
-  [{ name: "json" }, { name: "query" }],
+  'jmespath_search',
+  [{ name: 'json' }, { name: 'query' }],
   (_ctx, { json, query }) => {
     return search(json, query as string);
   }

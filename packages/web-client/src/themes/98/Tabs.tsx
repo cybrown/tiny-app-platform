96497a5e56338e8529d8d9bf5a1c588ee8efcb31
Cy Brown <cy.brown59@gmail.com>
2024-06-22 02:59:09 +0200

    fix(tabs): Fixed layout of 98 theme Tabs widget

--- packages/web-client/src/themes/98/Tabs.tsx
+++ packages/web-client/src/themes/98/Tabs.tsx
@@ -3,22 +3,24 @@ import styles from "./Tabs.module.css";
 
 export default function Tabs({ value, onChange, tabs, after }: TabsProps) {
   return (
-    <menu className={styles.Tabs} role="tablist">
-      {tabs.map((tab, index) => (
-        <li role="tab" key={index} aria-selected={tab.value === value}>
-          <a
-            href="#tabs"
-            onClick={(e) => {
-              onChange(tab.value);
-              e.preventDefault();
-            }}
-          >
-            {tab.label}
-          </a>
-        </li>
-      ))}
-      {after ? <div style={{ flexGrow: 1 }}></div> : null}
-      {after}
-    </menu>
+    <div className={styles.Tabs}>
+      <menu role="tablist">
+        {tabs.map((tab, index) => (
+          <li role="tab" key={index} aria-selected={tab.value === value}>
+            <a
+              href="#tabs"
+              onClick={(e) => {
+                onChange(tab.value);
+                e.preventDefault();
+              }}
+            >
+              {tab.label}
+            </a>
+          </li>
+        ))}
+        {after ? <div style={{ flexGrow: 1 }}></div> : null}
+        {after}
+      </menu>
+    </div>
   );
 }

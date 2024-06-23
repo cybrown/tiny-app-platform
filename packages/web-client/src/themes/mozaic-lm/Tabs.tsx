ddfb557bc443e6f8fa6d3d40319e6e43d792f92e
Cy Brown <cy.brown59@gmail.com>
2024-06-23 14:31:35 +0200

    fix(style): Fixed mozaic-lm theme Tabs vertical scrolling

--- packages/web-client/src/themes/mozaic-lm/Tabs.tsx
+++ packages/web-client/src/themes/mozaic-lm/Tabs.tsx
@@ -3,28 +3,34 @@ import styles from "./Tabs.module.css";
 
 export function Tabs({ tabs, value, onChange, after }: TabsProps) {
   return (
-    <div className={"mc-tabs " + styles.Tabs}>
-      <ul className="mc-tabs__nav" role="tablist" aria-label="Example of Tabs">
-        {tabs.map((tab) => (
-          <li className="mc-tabs__item" role="presentation" key={tab.value}>
-            <a
-              href="#/tab"
-              onClick={(e) => {
-                onChange(tab.value);
-                e.preventDefault();
-              }}
-              role="tab"
-              aria-selected={value === tab.value ? "true" : undefined}
-              className={`mc-tabs__element ${
-                value === tab.value ? "mc-tabs__element--selected" : ""
-              }`}
-            >
-              {tab.label}
-            </a>
-          </li>
-        ))}
-      </ul>
-      {after}
+    <div className={styles.Tabs}>
+      <div className="mc-tabs">
+        <ul
+          className="mc-tabs__nav"
+          role="tablist"
+          aria-label="Example of Tabs"
+        >
+          {tabs.map((tab) => (
+            <li className="mc-tabs__item" role="presentation" key={tab.value}>
+              <a
+                href="#/tab"
+                onClick={(e) => {
+                  onChange(tab.value);
+                  e.preventDefault();
+                }}
+                role="tab"
+                aria-selected={value === tab.value ? "true" : undefined}
+                className={`mc-tabs__element ${
+                  value === tab.value ? "mc-tabs__element--selected" : ""
+                }`}
+              >
+                {tab.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        {after}
+      </div>
     </div>
   );
 }

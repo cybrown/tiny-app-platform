53ce0068d7b791c27454b2bb562bc94d3848ca3f
Cy Brown <cy.brown59@gmail.com>
2025-02-22 00:56:17 +0100

    feat(test): Added some info logs during tests

--- packages/tal-test/scripts/generate-actual.sh
+++ packages/tal-test/scripts/generate-actual.sh
@@ -6,6 +6,7 @@ ls test-sources/*.tas | while read -r path
 do
   name_with_extension=$(basename "$path")
   name="${name_with_extension%.*}"
+  echo "Generate $name"
   node ./tal-parse-to-json.mjs < "test-sources/$name.tas" > "actual/$name.json"
   node ./tal-format.mjs < "test-sources/$name.tas" > "actual/$name.tas"
   node ./tal-walk-to-xml.mjs < "test-sources/$name.tas" > "actual/$name.xml"
--- packages/tal-test/test.sh
+++ packages/tal-test/test.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+echo "Generate actual"
 ./scripts/generate-actual.sh
+
+echo "Compare actual"
 ./scripts/compare-actual.sh

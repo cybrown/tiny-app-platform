21930ce975230f67574dc367f37891516f8aadab
Cy Brown <cy.brown59@gmail.com>
2025-02-24 21:19:05 +0100

    feat(image): Better image size option for Image widget

--- packages/web-client/src/widgets/Image.tsx
+++ packages/web-client/src/widgets/Image.tsx
@@ -4,14 +4,23 @@ import styles from "./Image.module.css";
 type ImageProps = {
   ctx: RuntimeContext;
   url: string;
-  fit?: boolean;
+  size?: "fit" | "original" | "standard";
 };
 
-export default function Image({ url, fit }: ImageProps) {
+export default function Image({ url, size: sizeFromProp }: ImageProps) {
+  const size = sizeFromProp ?? "standard";
   return (
     <div>
       <img
-        className={`${styles.Image} ${fit ? styles.fitted : ""}`}
+        className={`${styles.Image} ${
+          size == "fit"
+            ? styles.sizeFitted
+            : size == "original"
+            ? styles.sizeOriginal
+            : size === "standard"
+            ? styles.sizeStandard
+            : ""
+        }`}
         src={url}
         alt="Default content"
       />
@@ -23,7 +32,6 @@ export const ImageDocumentation: WidgetDocumentation<ImageProps> = {
   description: "An image from a URL",
   props: {
     url: "URL to the image",
-    fit:
-      "Allow the image to use its container size, use inside a sized Box to control the image size",
+    size: "Image size: fit: Allow the image to use its container size, use inside a sized Box to control the image size, original: keep original image size",
   },
 };

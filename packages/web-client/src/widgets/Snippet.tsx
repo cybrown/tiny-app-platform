8a72c0f1a01dddda263457aa276f14b28056e1ae
Cy Brown <cy.brown59@gmail.com>
2025-10-12 19:09:51 +0200

    feat(UI): Update Snippet widget to use Overlay widget instead of custom code for full screen viewer

--- packages/web-client/src/widgets/Snippet.tsx
+++ packages/web-client/src/widgets/Snippet.tsx
@@ -20,6 +20,8 @@ import "prismjs/components/prism-typescript.min";
 import "prismjs/components/prism-yaml.min";
 import usePressEscape from "./internal/usePressEscape";
 import { Button, Container } from "../theme";
+import LowLevelOverlay from "./internal/LowLevelOverlay";
+import { WindowFrame } from "../theme";
 
 type SnippetProps = {
   text: string;
@@ -77,28 +79,38 @@ export default function Snippet({
     <Container height={noMaxHeight ? undefined : viewMore ? 16 : 8}>
       <div className={styles.Snippet}>
         {fullScreen ? (
-          <div className={styles.fullScreen}>
-            <div className={styles.floatingFullscreenToolbar}>
-              <Button onClick={copyClickHandler} text="Copy" />
-              {format ? (
-                <Button
-                  onClick={copyFormattedClickHandler}
-                  text="Copy formatted"
-                />
-              ) : null}
-              <Button onClick={toggleFullScreen} text="Close" />
-            </div>
-            <div className={styles.scroller}>
-              {htmlToRender ? (
-                <pre
-                  className={styles.preFullscreen}
-                  dangerouslySetInnerHTML={{ __html: htmlToRender }}
-                />
-              ) : (
-                <pre className={styles.preFullscreen}>{textToRender}</pre>
-              )}
-            </div>
-          </div>
+          <>
+            <LowLevelOverlay modal onClose={toggleFullScreen} size="xl">
+              <WindowFrame
+                modal
+                size="xl"
+                title="Snippet"
+                onClose={toggleFullScreen}
+              >
+                <div className={styles.snippetContainer}>
+                  <div className={styles.floatingFullscreenToolbar}>
+                    <Button onClick={copyClickHandler} text="Copy" />
+                    {format ? (
+                      <Button
+                        onClick={copyFormattedClickHandler}
+                        text="Copy formatted"
+                      />
+                    ) : null}
+                  </div>
+                  <div className={styles.snippetContent}>
+                    {htmlToRender ? (
+                      <pre
+                        className={styles.preFullscreen}
+                        dangerouslySetInnerHTML={{ __html: htmlToRender }}
+                      />
+                    ) : (
+                      <pre className={styles.preFullscreen}>{textToRender}</pre>
+                    )}
+                  </div>
+                </div>
+              </WindowFrame>
+            </LowLevelOverlay>
+          </>
         ) : (
           <>
             <div className={styles.floatingToolBar}>
